dc3e76ce7e4fef420e9eba3eb9d0878d65ddcc82
Cesar Palo <cesarpalo@MacBook-Air-de-Cesar.local>
2026-04-27 00:59:28 -0500

    poda ok

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-...'
 DEBUG = True
-ALLOWED_HOSTS = ['localhost','192.168.0.3','38.250.176.122','192.168.100.103']  # en dev; en prod pon tu dominio/IP
+ALLOWED_HOSTS = ['localhost','192.168.0.3','38.250.176.122','192.168.100.103', '192.168.0.197']  # en dev; en prod pon tu dominio/IP
 
 INSTALLED_APPS = [
     'django.contrib.admin',
